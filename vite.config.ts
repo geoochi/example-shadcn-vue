408b1ac1a3b684c075493d9d85dd51505450caee
Geoo Chi <geoochi@geoochi.com>
2025-04-05 20:42:54 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import Pages from 'vite-plugin-pages'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  resolve: { alias: { '@': '/src' } },
+  plugins: [
+    tailwindcss(),
+    Vue({ include: [/\.vue$/] }),
+    Pages({ extensions: ['vue'] }),
+    Components({ extensions: ['vue'], include: [/\.vue$/] }),
+  ],
+})
