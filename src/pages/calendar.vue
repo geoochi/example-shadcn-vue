fbc4feb3cbeec51078116dbda11ac48fcf6d51b5
Geoo Chi <geoochi@geoochi.com>
2025-04-06 14:03:00 +0800

    fix calendar

--- src/pages/calendar.vue
+++ src/pages/calendar.vue
@@ -1,43 +1,34 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
-import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import { toast } from 'vue-sonner'
-import { DateFormatter, getLocalTimeZone, parseDate, today } from '@internationalized/date'
+import {
+  CalendarDate,
+  DateFormatter,
+  getLocalTimeZone,
+  parseDate,
+  today,
+} from '@internationalized/date'
 import { toTypedSchema } from '@vee-validate/zod'
 import { CalendarIcon } from 'lucide-vue-next'
-import { toDate, createYear, createDecade } from 'reka-ui/date'
+import { toDate } from 'reka-ui/date'
 import { useForm } from 'vee-validate'
-import { computed, type HTMLAttributes, type Ref } from 'vue'
+import { computed, h, ref } from 'vue'
 import { z } from 'zod'
-import { useVModel } from '@vueuse/core'
-import { type DateValue } from 'reka-ui/date'
-import { CalendarRoot, CalendarRootEmits, CalendarRootProps, useDateFormatter, useForwardPropsEmits } from 'reka-ui'
-
-const props = withDefaults(defineProps<CalendarRootProps & { class?: HTMLAttributes['class'] }>(), {
-  modelValue: undefined,
-  placeholder() {
-    return today(getLocalTimeZone())
-  },
-  weekdayFormat: 'short',
-})
-const emits = defineEmits<CalendarRootEmits>()
-
-const delegatedProps = computed(() => {
-  const { class: _, placeholder: __, ...delegated } = props
-
-  return delegated
-})
-
-const placeholder = useVModel(props, 'modelValue', emits, {
-  passive: true,
-  defaultValue: today(getLocalTimeZone()),
-}) as Ref<DateValue>
-
-const forwarded = useForwardPropsEmits(delegatedProps, emits)
-
-const formatter = useDateFormatter('en')
 
 const df = new DateFormatter('en-US', {
   dateStyle: 'long',
@@ -49,6 +40,8 @@ const formSchema = toTypedSchema(
   })
 )
 
+const placeholder = ref()
+
 const { handleSubmit, setFieldValue, values } = useForm({
   validationSchema: formSchema,
 })
@@ -59,7 +52,7 @@ const value = computed({
 })
 
 const onSubmit = handleSubmit(values => {
-  toast('You submitted the following values:', { description: values.dob })
+  toast(`selected date:${values.dob}`)
 })
 </script>
 
@@ -73,21 +66,31 @@ const onSubmit = handleSubmit(values => {
             <FormControl>
               <Button
                 variant="outline"
-                :class="cn('w-[240px] ps-3 text-start font-normal', !value && 'text-muted-foreground')"
+                :class="
+                  cn(
+                    'w-[240px] ps-3 text-start font-normal',
+                    !value && 'text-muted-foreground'
+                  )
+                "
               >
-                <span>{{ value ? df.format(toDate(value)) : 'Pick a date' }}</span>
+                <span>{{
+                  value ? df.format(toDate(value)) : 'Pick a date'
+                }}</span>
                 <CalendarIcon class="ms-auto h-4 w-4 opacity-50" />
               </Button>
               <input hidden />
             </FormControl>
           </PopoverTrigger>
           <PopoverContent class="w-auto p-0">
-            <CalendarRoot
-              v-slot="{ date, grid, weekDays }"
+            <Calendar
               v-model:placeholder="placeholder"
               v-model="value"
+              calendar-label="Date of birth"
+              initial-focus
+              :min-value="new CalendarDate(1900, 1, 1)"
+              :max-value="today(getLocalTimeZone())"
               @update:model-value="
-                (v: DateValue) => {
+                v => {
                   if (v) {
                     setFieldValue('dob', v.toString())
                   } else {
@@ -95,91 +98,12 @@ const onSubmit = handleSubmit(values => {
                   }
                 }
               "
-              v-bind="forwarded"
-              :class="cn('rounded-md border p-3', props.class)"
-            >
-              <CalendarHeader>
-                <CalendarHeading class="flex w-full items-center justify-between gap-2">
-                  <Select
-                    :default-value="placeholder.month.toString()"
-                    @update:model-value="
-                        (v: string) => {
-                        if (!v || !placeholder) return
-                        if (Number(v) === placeholder?.month) return
-                        placeholder = placeholder.set({
-                            month: Number(v),
-                        })
-                        }
-                    "
-                  >
-                    <SelectTrigger aria-label="Select month" class="w-[60%]">
-                      <SelectValue placeholder="Select month" />
-                    </SelectTrigger>
-                    <SelectContent class="max-h-[200px]">
-                      <SelectItem
-                        v-for="month in createYear({ dateObj: date })"
-                        :key="month.toString()"
-                        :value="month.month.toString()"
-                      >
-                        {{ formatter.custom(toDate(month), { month: 'long' }) }}
-                      </SelectItem>
-                    </SelectContent>
-                  </Select>
-
-                  <Select
-                    :default-value="placeholder.year.toString()"
-                    @update:model-value="
-                        (v: string) => {
-                        if (!v || !placeholder) return
-                        if (Number(v) === placeholder?.year) return
-                        placeholder = placeholder.set({
-                            year: Number(v),
-                        })
-                        }
-                    "
-                  >
-                    <SelectTrigger aria-label="Select year" class="w-[40%]">
-                      <SelectValue placeholder="Select year" />
-                    </SelectTrigger>
-                    <SelectContent class="max-h-[200px]">
-                      <SelectItem
-                        v-for="yearValue in createDecade({ dateObj: date, startIndex: -10, endIndex: 10 })"
-                        :key="yearValue.toString()"
-                        :value="yearValue.year.toString()"
-                      >
-                        {{ yearValue.year }}
-                      </SelectItem>
-                    </SelectContent>
-                  </Select>
-                </CalendarHeading>
-              </CalendarHeader>
-
-              <div class="flex flex-col space-y-4 pt-4 sm:flex-row sm:gap-x-4 sm:gap-y-0">
-                <CalendarGrid v-for="month in grid" :key="month.value.toString()">
-                  <CalendarGridHead>
-                    <CalendarGridRow>
-                      <CalendarHeadCell v-for="day in weekDays" :key="day">
-                        {{ day }}
-                      </CalendarHeadCell>
-                    </CalendarGridRow>
-                  </CalendarGridHead>
-                  <CalendarGridBody class="grid">
-                    <CalendarGridRow
-                      v-for="(weekDates, index) in month.rows"
-                      :key="`weekDate-${index}`"
-                      class="mt-2 w-full"
-                    >
-                      <CalendarCell v-for="weekDate in weekDates" :key="weekDate.toString()" :date="weekDate">
-                        <CalendarCellTrigger :day="weekDate" :month="month.value" />
-                      </CalendarCell>
-                    </CalendarGridRow>
-                  </CalendarGridBody>
-                </CalendarGrid>
-              </div>
-            </CalendarRoot>
+            />
           </PopoverContent>
         </Popover>
-        <FormDescription> Your date of birth is used to calculate your age. </FormDescription>
+        <FormDescription>
+          Your date of birth is used to calculate your age.
+        </FormDescription>
         <FormMessage />
       </FormItem>
     </FormField>
